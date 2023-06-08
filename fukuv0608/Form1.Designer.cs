b57c810475ce4213497359427601f4e5e8583334
YuTanaka <tanakaedu@users.noreply.github.com>
2023-06-08 16:10:14 +0900

    乱数実装

--- fukuv0608/Form1.Designer.cs
+++ fukuv0608/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(322, 164);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 45);
+            label1.TabIndex = 0;
+            label1.Text = "★";
+            label1.Click += label1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 50;
+            timer1.Tick += timer1_Tick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Yu Gothic UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(196, 177);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 32);
+            label2.TabIndex = 1;
+            label2.Text = "(・ω・)";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Yu Gothic UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(459, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(37, 45);
+            label3.TabIndex = 2;
+            label3.Text = "0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(535, 324);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label2;
+        private Label label3;
     }
 }
--- fukuv0608/Form1.cs
+++ fukuv0608/Form1.cs
@@ -2,9 +2,76 @@ namespace fukuv0608
 {
     public partial class Form1 : Form
     {
+        int vx = rand.Next(-5, 6);
+        int vy = rand.Next(-5, 6);
+        int iTime = 0;
+        static Random rand = new Random();
+
         public Form1()
         {
             InitializeComponent();
+            label1.Left = rand.Next(ClientSize.Width - label1.Width);
+            label1.Top = rand.Next(ClientSize.Height - label1.Height);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            iTime++;
+            label3.Text = $"{iTime}";
+
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            else if (label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+
+            if (label1.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+            else if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+
+            // 変数mposを宣言して、マウスのフォーム座標を取り出す
+            //// 1. MousePositionにマウス座標のスクリーン左上からのX、Yが入っている
+            Text = $"{MousePosition.X}, {MousePosition.Y}";
+
+            //// 2. 変数fposを宣言して、PointToClient()でスクリーン座標をフォーム座標に変換
+            var fpos = PointToClient(MousePosition);
+
+            // ラベルに座標を表示
+            //// 変換したフォーム座標は、fpos.X、fpos.Yで取り出せる
+            //label1.Text = $"{fpos.X}, {fpos.Y}";
+
+            // label2.Widthでラベルの幅
+            // label2.Heightでラベルの高さ
+            // マウスカーソルの位置がLabel2の中央になるようにする
+            label2.Left = fpos.X - label2.Width / 2;
+            label2.Top = fpos.Y - label2.Height / 2;
+
+            // 当たり判定
+            if ((fpos.X > label1.Left)
+                && (fpos.X < label1.Right)
+                && (fpos.Y > label1.Top)
+                && (fpos.Y < label1.Bottom))
+            {
+                timer1.Enabled = false;
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = "Tanaka Yu";
         }
     }
 }
